d7a85de3c7839012556181b7805ad95def2bf390
Luke de Waal <lr.de.waal.01@gmail.com>
2019-04-10 16:52:18 +0200

    .

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Flight Mechanics :: Atmosphere Modelling',
+    'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: GNU GPLv3',   # Again, pick a license
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
